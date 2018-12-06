43a3a42a3ac4647d462fa7916150a2b6ceebca09
James Gotsell <j.e.gotsell@gmail.com>
2018-12-06 22:07:16 +0000

    my solution for lesson one

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ import { restRouter } from './api'
 import { connect } from './db'
 import { signin, protect } from './api/modules/auth'
 // Declare an app from express
-const app = null
+const app = express()
+
+
+app.get('/', (req,res) => {
+    res.send("hello world")
+
+})
 
 export default app
